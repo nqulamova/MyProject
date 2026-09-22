d060a801da93ec9e9b01690505b7698061ece68d
nqulamova <nqulamova1@std.beu.edu.az>
2026-09-22 21:58:05 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Adinizi daxil edin: ");
+        string ad = Console.ReadLine();
+
+        Console.Write("Yasinizi daxil edin: ");
+        int yas = int.Parse(Console.ReadLine());
+
+        Console.Write("Boyunuzu daxil edin: ");
+        double boy = double.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Salam, menim adim {ad}-dir. Menim {yas} yasim var, boyum {boy} m-dir.");
+    }
+}
